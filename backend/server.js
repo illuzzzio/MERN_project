7ae5bec7d83d96234540c1503d4c97ffd8bebc44
Your Name <animusaaa@gmail.com>
2025-09-17 00:45:57 +0530

    nodemon json and api routes syntax done

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+import express from "express";
+
+const app = express();
+
+
+app.get("/api/notes", (req,res)=>{
+  res.status(200).send("you got 25 notes");
+})
+
+app.post("/api/notes/:id",(req,res)=>{
+  res.status(201).json({message:"Note 1 created successfully "});
+})
+
+
+app.put("/api/notes/:id",(req,res)=>{
+  res.status(200).json({message:"Note added successfully"})
+})
+
+app.delete("/api/notes/:id", (req,res)=>{
+  res.status(200).json({message:"The notes has been deleted successfully"})
+})
+
+app.listen(5001, ()=>{
+  console.log("The server as been started successfully at the PORT 5001")
+})
